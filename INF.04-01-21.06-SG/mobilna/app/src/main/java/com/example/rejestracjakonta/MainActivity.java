97531ef8ed6c68a6fee038b4800135352cd6c1d5
Alicja Gorgoń <alicja.pegiel@zskocjan.pl>
2025-09-09 12:14:17 +0200

    Zrobiona apka

--- INF.04-01-21.06-SG/mobilna/app/src/main/java/com/example/rejestracjakonta/MainActivity.java
+++ INF.04-01-21.06-SG/mobilna/app/src/main/java/com/example/rejestracjakonta/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.rejestracjakonta;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,35 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView komunikatTV = findViewById(R.id.komunikat);
+        EditText emailET = findViewById(R.id.email);
+        EditText hasloET = findViewById(R.id.haslo1);
+        EditText powtorzHasloET = findViewById(R.id.haslo2);
+        Button przyciskZatwierdz = findViewById(R.id.zatwierdz);
+
+        przyciskZatwierdz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String email = emailET.getText().toString();
+                    String haslo = hasloET.getText().toString();
+                    String powtorzHaslo = powtorzHasloET.getText().toString();
+                    if (!(email.contains("@"))) {
+                        komunikatTV.setText("Nieprawidłowy adres e-mail");
+                    } else {
+                        if (!haslo.equals(powtorzHaslo) || haslo.isEmpty()) {
+                            komunikatTV.setText("Hasła się różnią");
+                        }
+                        else {
+                            komunikatTV.setText("Witaj " + email);
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
         });
     }
 }
