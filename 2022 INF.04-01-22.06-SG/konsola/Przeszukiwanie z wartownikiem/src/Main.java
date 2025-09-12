4f3bee2bd36278d5491bef96a6b713b1d76a7c24
Alicja Gorgoń <alicja.pegiel@zskocjan.pl>
2025-09-12 11:07:44 +0200

    Aplikacja konsolowa

--- 2022 INF.04-01-22.06-SG/konsola/Przeszukiwanie z wartownikiem/src/Main.java
+++ 2022 INF.04-01-22.06-SG/konsola/Przeszukiwanie z wartownikiem/src/Main.java
@@ -1,7 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        PrzeszukiwanieZWartownikiem algorytm = new PrzeszukiwanieZWartownikiem();
+        algorytm.uzupelnijTablice();
+        System.out.print("Podaj liczbę do wyszukania: ");
+        Scanner scanner = new Scanner(System.in);
+        int element = scanner.nextInt();
+        algorytm.wynikDzialania(element);
 
+        System.out.println("\nTESTY:");
+        System.out.println("Liczba poza zakresem: -1");
+        algorytm.wynikDzialania(-1);
+        System.out.println("Liczba poza zakresem: 101");
+        algorytm.wynikDzialania(101);
+        System.out.println("Liczba z połowy zakresu: 50");
+        algorytm.wynikDzialania(50);
     }
 }
--- 2022 INF.04-01-22.06-SG/konsola/Przeszukiwanie z wartownikiem/src/PrzeszukiwanieZWartownikiem.java
+++ 2022 INF.04-01-22.06-SG/konsola/Przeszukiwanie z wartownikiem/src/PrzeszukiwanieZWartownikiem.java
@@ -1,3 +1,44 @@
-public class PrzeszukiwanieZWartownikiem {
+import java.util.Random;
 
+public class PrzeszukiwanieZWartownikiem {
+    public int rozmiarTablicy = 50;
+    private final int[] tablicaLiczb = new int[rozmiarTablicy+1];
+
+    public void uzupelnijTablice() {
+        Random random = new Random();
+        for (int i = 0; i < rozmiarTablicy; i++) {
+            tablicaLiczb[i] = random.nextInt(100) + 1;
+        }
+    }
+
+    public void wyswietlTablice() {
+        System.out.print("Zawartość tablicy: ");
+        for (int i = 0; i < rozmiarTablicy-1; i++) {
+            System.out.print(tablicaLiczb[i] + ", ");
+        }
+        System.out.println(tablicaLiczb[rozmiarTablicy-1]);
+    }
+
+    public int szukajElementu(int szukanyElement) {
+        int indeksElementu = rozmiarTablicy;
+        tablicaLiczb[indeksElementu] = szukanyElement;
+        for (int i = 0; i < rozmiarTablicy; i++) {
+            if (tablicaLiczb[i] == szukanyElement) {
+                indeksElementu = i;
+                break;
+            }
+        }
+        return indeksElementu;
+    }
+
+    public void wynikDzialania(int element) {
+        int wartownik = szukajElementu(element);
+        wyswietlTablice();
+        if (wartownik != rozmiarTablicy) {
+            System.out.println("Indeks pierwszego wystąpienia elementu " + element + " to " + wartownik);
+        }
+        else {
+            System.out.println("Element " + element + " nie znajduje się w tablicy");
+        }
+    }
 }
